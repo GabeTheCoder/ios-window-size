1554e8289c4ce47c956fabb8d05735375b1d3d21
Gabe The Coder <gabethecoder@icloud.com>
2019-11-04 16:25:33 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+let currentHeight = 0;
+let currentHeightIndex = 0;
+
+let transitionComplete = false;
+
+const calculateHeight = () => {
+    if (currentHeightIndex > 100) return;
+
+    if (currentHeight !== window.innerHeight) {
+        currentHeight = window.innerHeight;
+        document.documentElement.style.setProperty('--vh', window.innerHeight + 'px');
+    }
+
+    currentHeightIndex += 1;
+};
+
+const update = () => calculateHeight();
+
+window.addEventListener('load', update);
+window.addEventListener('resize', update);
+
